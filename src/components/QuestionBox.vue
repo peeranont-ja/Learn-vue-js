72dfe89b52fa2fccd00a8b3d531c31cd1e890ae7
Unknown <peeranont.ja@tnis.com>
2019-06-26 00:53:19 +0700

    Add on click handler

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -6,7 +6,11 @@
       <hr class="my-4">
 
       <b-list-group>
-        <b-list-group-item v-for="(answer, index) in answers" :key="index">{{ answer }}</b-list-group-item>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+        >{{ answer }}</b-list-group-item>
       </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
@@ -21,11 +25,22 @@ export default {
     currentQuestion: Object,
     next: Function
   },
+  data() {
+    return {
+      selectedIndex: null
+    };
+  },
   computed: {
     answers() {
       let answers = [...this.currentQuestion.incorrect_answers];
       answers.push(this.currentQuestion.correct_answer);
       return answers;
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+      console.log(index);
     }
   },
   mounted() {
@@ -38,6 +53,11 @@ export default {
 <style scoped>
 .list-group {
   margin-bottom: 15px;
+}
+
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
 }
 
 .btn {
